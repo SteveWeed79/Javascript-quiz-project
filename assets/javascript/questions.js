828572f376d5d2a1a488c5b77aee0ba32d0acd03
unknown <steveweed1979@gmail.com>
2022-07-06 23:51:50 -0500

    Worked on Javascript for question selection

---
+++ assets/javascript/questions.js
@@ -0,0 +1,31 @@
+
+var questions = [
+    {
+        questionText: "How to declare a variable in xcv?",
+        choices: ["var", "for", "while", "if"],
+        answer: "var"
+    },
+    {
+        questionText: "How do you speify a block of code to be executed if its value is true",
+        choices: ["var", "for", "while", "if"],
+        answer: "if"
+    },
+    {
+        questionText: "Which loop run through a block of code a number of times?",
+        choices: ["var", "for", "while", "if"],
+        answer: "for"
+    },
+    {
+        questionText: "Which loops runs as long as a speified condtion is true?",
+        choices: ["var", "for", "while", "if"],
+        answer: "while"
+    },
+    {
+        questionText: "How to declare a variable in xcv?",
+        choices: ["var", "for", "while", "if"],
+        answer: "var"
+    }
+]
+
+
+
---
+++ assets/javascript/script.js
@@ -0,0 +1,16 @@
+
+var startQuiz = document.querySelector("#start");
+
+var selectedQuestion = [];
+
+
+function pickQuestion(selectedQuestion) {
+    let i = 0
+    if (i = 0) {
+        var printQuestion = questions
+    }
+    console.log(printQuestion)
+}
+
+startQuiz.addEventListener("click", pickQuestion);
+//document.getElementById("questionPlacement").innerHTML = printQuestion;
