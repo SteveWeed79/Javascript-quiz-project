a2c5feafb14115dc1c413c7aeae802eb504df6cb
unknown <stevewee1979@gmail.com>
2022-07-07 22:20:03 -0500

    Worked on javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 
 var startQuiz = document.querySelector("#start");
+var oneClick = document.querySelector("#buttonOne")
 
 var selectedQuestion = [];
 
@@ -14,6 +15,23 @@ function pickQuestion() {
     document.getElementById("buttonFour").innerHTML = selectedQuestion.choices[3];
     console.log(selectedQuestion)
     i++
+    oneClick.addEventListener("click", nextQuestion);
+    function nextQuestion() {
+        if (i >= 5) {
+            var selectedQuestion = questions[i];
+            document.getElementById("questionContainer").innerHTML = selectedQuestion.questionText;
+            document.getElementById("buttonOne").innerHTML = selectedQuestion.choices[0];
+            document.getElementById("buttonTwo").innerHTML = selectedQuestion.choices[1];
+            document.getElementById("buttonThree").innerHTML = selectedQuestion.choices[2];
+            document.getElementById("buttonFour").innerHTML = selectedQuestion.choices[3];
+            console.log(selectedQuestion)
+             i++
+             nextQuestion()
+        } else {
+            return
+        }
+    }
+
 }
 
 
