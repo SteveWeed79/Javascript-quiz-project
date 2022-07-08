fd3f9cd2612db2ed6ebade0c2b6cc9123bca33ca
unknown <steveweed1979@gmail.com>
2022-07-07 23:41:59 -0500

    Working on javascript on buttons and logic.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 
 var startQuiz = document.querySelector("#start");
-var oneClick = document.querySelector("#buttonOne")
+var oneClick = document.querySelector(".button");
+var answerButton = [];
 
 var selectedQuestion = [];
 
@@ -13,26 +14,24 @@ function pickQuestion() {
     document.getElementById("buttonTwo").innerHTML = selectedQuestion.choices[1];
     document.getElementById("buttonThree").innerHTML = selectedQuestion.choices[2];
     document.getElementById("buttonFour").innerHTML = selectedQuestion.choices[3];
-    console.log(selectedQuestion)
+    var selectedAnswer = selectedQuestion.answer;
     i++
-    oneClick.addEventListener("click", nextQuestion);
-    function nextQuestion() {
-        if (i >= 5) {
-            var selectedQuestion = questions[i];
-            document.getElementById("questionContainer").innerHTML = selectedQuestion.questionText;
-            document.getElementById("buttonOne").innerHTML = selectedQuestion.choices[0];
-            document.getElementById("buttonTwo").innerHTML = selectedQuestion.choices[1];
-            document.getElementById("buttonThree").innerHTML = selectedQuestion.choices[2];
-            document.getElementById("buttonFour").innerHTML = selectedQuestion.choices[3];
-            console.log(selectedQuestion)
-             i++
-             nextQuestion()
-        } else {
-            return
-        }
-    }
 
+    oneClick.addEventListener("click", nextQuestion);
+
+
+    function nextQuestion() {
+
+        if (selectedQuestion.choices = selectedAnswer) {
+            document.getElementById("rightWrong").innerHTML = "Correct!";
+            pickQuestion()
+        } else {
+            document.getElementById("rightWrong").innerHTML = "Wrong!";
+        }
+
+    }
 }
+
 
 
 
