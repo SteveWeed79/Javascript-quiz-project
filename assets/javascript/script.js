2d51c86b080908e492054227d0c51fd8d4e16842
unknown <stevewee1979@gmail.com>
2022-07-08 21:58:18 -0500

    Worked on button.target.event and timer.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,78 @@
 
 var startQuiz = document.querySelector("#start");
-var oneClick = document.querySelector(".button");
+var btnContainerEl = document.querySelector(".parent")
 var answerButton = [];
-
 var selectedQuestion = [];
+let qCounter = 0
+var timeLeft = 30;
+var timeInterval;
+var timerEl = document.getElementById("start")
 
+function startGame() {
+    var startQuiz = document.querySelector("#start");
+    countdown()
+    pickQuestion()
+    startQuiz.removeEventListener("click", startGame, false);
+    btnContainerEl.addEventListener("click", nextQuestion)
+}
+
+function countdown() {
+    
+    timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.innerHTML = timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+      endGame()
+      }
+    }, 1000);
+}
 
 function pickQuestion() {
-    let i = 0
-    var selectedQuestion = questions[i];
-    document.getElementById("questionContainer").innerHTML = selectedQuestion.questionText;
-    document.getElementById("buttonOne").innerHTML = selectedQuestion.choices[0];
-    document.getElementById("buttonTwo").innerHTML = selectedQuestion.choices[1];
-    document.getElementById("buttonThree").innerHTML = selectedQuestion.choices[2];
-    document.getElementById("buttonFour").innerHTML = selectedQuestion.choices[3];
-    var selectedAnswer = selectedQuestion.answer;
-    i++
-
-    oneClick.addEventListener("click", nextQuestion);
+    
+    var selectedQuestion = questions[qCounter];
+    document.getElementById("questionContainer").textContent = selectedQuestion.questionText;
+    document.getElementById("buttonOne").textContent = selectedQuestion.choices[0];
+    document.getElementById("buttonTwo").textContent = selectedQuestion.choices[1];
+    document.getElementById("buttonThree").textContent = selectedQuestion.choices[2];
+    document.getElementById("buttonFour").textContent = selectedQuestion.choices[3];
 
 
-    function nextQuestion() {
+}
 
-        if (selectedQuestion.choices = selectedAnswer) {
+    function nextQuestion(event) {
+        if (event.target.matches("button")){
+            var selectedQuestion = questions[qCounter];
+            var selectedAnswer = selectedQuestion.answer;
+        if (event.target.textContent === selectedAnswer) {
             document.getElementById("rightWrong").innerHTML = "Correct!";
             pickQuestion()
         } else {
             document.getElementById("rightWrong").innerHTML = "Wrong!";
+            timeLeft -= 10
         }
-
+        qCounter++
+        if (qCounter === questions.length - 1) {
+            endGame()
+        } else {
+        pickQuestion();
+        }
     }
-}
+    }
+    function endGame() {
+        timerEl.textContent = '';
+  
+        clearInterval(timeInterval);
+
+        displayMessage();
+    }
 
 
 
 
-startQuiz.addEventListener("click", pickQuestion);
+startQuiz.addEventListener("click", startGame);
+
 //document.getElementById("questionPlacement").innerHTML = printQuestion;
